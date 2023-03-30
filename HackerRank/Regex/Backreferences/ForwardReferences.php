6b125e4a3ad0c5ae30b3e63ce701544afe238ce6
rinkerm <rinkerm915@gmail.com>
2023-03-29 23:34:01 -0700

    Regex Practice

---
+++ HackerRank/Regex/Backreferences/ForwardReferences.php
@@ -0,0 +1,15 @@
+<?php
+
+$Regex_Pattern = '/^(tac){2,}((?<!tic)tic(?!tic)|tac)*$/'; //Do not delete '/'. Replace __________ with your regex.
+
+$handle = fopen ("php://stdin","r");
+$Test_String = fgets($handle);
+if(preg_match($Regex_Pattern, $Test_String, $output_array)){
+    print ("true");
+} else {
+    print ("false");
+}
+
+fclose($handle);
+?>
+
