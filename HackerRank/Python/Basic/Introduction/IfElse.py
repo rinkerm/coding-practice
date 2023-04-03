ca014f397d2cc414a2d42c06bbf8a865368776eb
rinkerm <rinkerm915@gmail.com>
2023-04-03 01:17:05 -0700

    HackerRank Python Basic Introduction Category

---
+++ HackerRank/Python/Basic/Introduction/IfElse.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n % 2 == 1 or (n%2==0 and (n >=6 and n  <= 20)):
+        print('Weird')
+    else:
+        print('Not Weird')
+
